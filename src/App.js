e6bc9ad9aec7c438594edbd86eb2b1f768a5fd64
prateek singh <prateek3693@gmail.com>
2021-11-21 21:16:21 +0530

    updated the files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to reload.
+          Welcome to React!
         </p>
         <a
           className="App-link"
